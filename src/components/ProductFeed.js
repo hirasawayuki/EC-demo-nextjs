4415f7ca4d629eeb6f2153d3b507ab0a04da166c
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-09-20 23:20:07 +0900

    fix product feed components

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,8 +1,18 @@
+import Product from "./Product"
+
 function ProductFeed({ products }) {
   return (
-    <div>
-      {products.map((product) => (
-        <p>{product.title}</p>
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3">
+      {products.map(({id, title, price, description, category, image}) => (
+        <Product
+          key={id}
+          id={id}
+          title={title}
+          price={price}
+          description={description}
+          category={category}
+          image={image}
+        />
       ))}
     </div>
   )
