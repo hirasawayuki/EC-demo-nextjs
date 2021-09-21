52fe559a8889e23125defde8447a3fad37cb16ea
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-09-21 22:37:02 +0900

    fix product feed design

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -2,8 +2,8 @@ import Product from "./Product"
 
 function ProductFeed({ products }) {
   return (
-    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3">
-      {products.map(({id, title, price, description, category, image}) => (
+    <div className="grid grid-flow-row-dense md:-mt-52 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto">
+      {products.slice(0, 4).map(({id, title, price, description, category, image}) => (
         <Product
           key={id}
           id={id}
@@ -14,6 +14,34 @@ function ProductFeed({ products }) {
           image={image}
         />
       ))}
+
+      <img className="md:col-span-full" src="https://links.papareact.com/dyz" alt="" />
+
+      <div className="md:col-span-2">
+        {products.slice(4, 5).map(({id, title, price, description, category, image}) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+      </div>
+
+        {products.slice(5, products.length).map(({id, title, price, description, category, image}) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
     </div>
   )
 }
