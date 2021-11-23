0aa53a7107b34542f4f968d02e63aa21a76cd135
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-11-23 18:55:56 +0900

    feature google signin

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,8 @@ import  {
   ShoppingCartIcon,
 } from "@heroicons/react/outline"
 
+import { signIn, signOut, useSession } from "next-auth/client"
+
 function Header() {
   return (
     <header>
@@ -23,7 +25,7 @@ function Header() {
           <SearchIcon className="h-12 p-4" />
         </div>
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div className="link">
+          <div onClick={signIn} className="cursor-pointer link">
             <p>Hello Amazon</p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
