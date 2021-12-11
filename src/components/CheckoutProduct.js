f33d8ae1b32ad076aa247b7871326d88acfd7dfa
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-12-11 22:11:52 +0900

    feat shopping basket details

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -1,5 +1,6 @@
-import {StarIcon} from "@heroicons/react/outline";
+import {StarIcon} from "@heroicons/react/solid";
 import Image from "next/image";
+import Currency from "react-currency-formatter"
 
 function CheckoutProduct({
   id,
@@ -14,6 +15,31 @@ function CheckoutProduct({
   return (
     <div className="grid grid-cols-5">
       <Image src={image} width={200} height={200} objectFit="contain" />
+      <div className="col-span-3 mx-5">
+        <p>{title}</p>
+        <div className="flex">
+          {Array(rating).fill().map((_, i) => (
+            <StarIcon key={i} className="h-5 text-yellow-500" />
+          ))}
+        </div>
+        <p className="text-xs my-2 line-clamp3">{description}</p>
+        <Currency quantity={price} currency="usd" />
+        {hasPrime && (
+          <div className="flex items-center space-x-2">
+            <img
+              loading="lazy"
+              className="w-12"
+              src="https://links.papareact.com/fdw"
+              alt=""
+            />
+            <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col space-y-2 my-auto justify-self-end">
+        <button className="button">Add to Basket</button>
+        <button className="button">Remove from Basket</button>
+      </div>
     </div>
   );
 }
