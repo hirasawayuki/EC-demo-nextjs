c29f175a17f6bfc5c293dc59a9f2affc4463b049
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-12-25 22:37:58 +0900

    change state library redux to recoil

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,13 @@ import  {
 
 import { signIn, signOut, useSession } from "next-auth/client"
 import { useRouter } from "next/router"
-import { useSelector } from "react-redux"
-import { selectItems } from "../slices/basketSlice"
+import {useRecoilValue} from "recoil";
+import {basketItemsQuantity} from "../app/state";
 
 function Header() {
   const [session] = useSession();
   const router = useRouter();
-  const items = useSelector(selectItems);
+  const quantity = useRecoilValue(basketItemsQuantity)
 
   return (
     <header>
@@ -45,7 +45,7 @@ function Header() {
           </div>
           <div onClick={() => router.push('/checkout')} className="relative link flex items-center">
             <span className="absolute top-0 right-0 md:right-11 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
-              {items.length}
+              {quantity}
             </span>
             <ShoppingCartIcon className="h-10"/>
             <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
