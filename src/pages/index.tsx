4bdb1cdec20e062493c6187a487e818e542f3ad3
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-12-29 00:05:39 +0900

    change pages from javascript to typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,13 @@ import Header from "../components/Header"
 import Banner from "../components/Banner"
 import ProductFeed from "../components/ProductFeed"
 import {getSession} from "next-auth/client";
+import { GetServerSidePropsContext } from "next";
 
-export default function Home({ products }) {
+type HomeProps = {
+  products: Product[];
+}
+
+export default function Home({ products }: HomeProps) {
   return (
     <div className="bg-gray-100">
       <Head>
@@ -19,7 +24,7 @@ export default function Home({ products }) {
   );
 }
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await getSession(context);
   const products = await fetch("https://fakestoreapi.com/products").then(
     (res) => res.json()
