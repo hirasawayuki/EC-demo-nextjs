37298547515e806b9a45125b3302a3dfb14751c7
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2021-12-30 13:14:07 +0900

    fix Order.tsx

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -1,35 +1,39 @@
 import moment from "moment";
 import Currency from "react-currency-formatter";
-import Image from "next/image";
 
-function Order({ id, amount, amountShipping, items, timestamp, images }: Order) {
+type OrderProps = {
+  order: Order;
+}
+
+const Order: React.VFC<OrderProps> = ({ order }) => {
   return (
     <div className="relative border rouded-md">
       <div className="flex items-center space-x-10 p-5 bg-gray-100 text-sm text-gray-600">
         <div>
           <p className="font-bold text-xs">ORDER PLACED</p>
-          <p>{moment.unix(timestamp).format('DD MM YYYY')}</p>
+          <p>{moment.unix(order.timestamp).format('DD MM YYYY')}</p>
         </div>
         <div>
           <p className="text-xs font-bold">TOTAL</p>
           <p>
-            <Currency quantity={amount} currency="USD" /> - Next Day Delivery{" "}
-            <Currency quantity={amountShipping} currency="USD" />
+            <Currency quantity={order.amount} currency="USD" /> - Next Day Delivery{" "}
+            <Currency quantity={order.amountShipping} currency="USD" />
           </p>
         </div>
         <p className="text-sm whitespace-nowrap sm:text-xl self-end flex-1 text-right text-blue-500">
-          {items.length} items
+          {order.items.length} items
         </p>
 
         <p className="absolute top-2 right-2 w-40 lg:w-72 truncate text-xs whitespace-nowrap">
-          ORDER # {id}
+          ORDER # {order.id}
         </p>
       </div>
 
       <div className="p-5 sm:p-10">
         <div className="flex space-x-6 overflow-x-auto">
-          {images.map(image => (
-            <Image key={image} src={image} alt="" className="h-20 object-contain sm:h-32" />
+          {order.images.map(image => (
+            /* eslint-disable  @next/next/no-img-element */
+            <img key={image} src={image} alt="" className="h-20 object-contain sm:h-32" />
           ))}
         </div>
       </div>
